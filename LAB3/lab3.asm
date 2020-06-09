c868af5a38e69c99c5e6e6de32dcfe5199c03561
Anna Kozlovskaya <a.kozlovskaya@corp.mail.ru>
2020-06-09 13:25:39 +0300

    asm labs full

--- LAB3/lab3.asm
+++ LAB3/lab3.asm
@@ -7,36 +7,34 @@
 .486
 .data
         N       dw      3
-		M       dw      4
-		MM      dw      5
+        M       dw      4
+	MM      dw      5
+
         Matrix  dw      1,   2,   3,  4
                 dw      5,   6,   7,  8
                 dw      9,  10,  11, 12
+
         Sum     dw      ?
 .code
- 
-main    proc
         mov     ax,     @data
         mov     ds,     ax
- 
-        ;вычислим длину строки
-        mov     dx,     [MM]
+
+        mov     dx,     [MM]            ;вычислим длину строки матрицы
         shl     dx,     1
 
         lea     bx,     [Matrix]
-        mov     si,     0       ;номер столбца, умноженное на размер элемента
+        mov     si,     0
         shl     si,     1
-        mov     ax,     0       ;сумма
-        mov     cx,     [N]     ;цикл по строкам (по 2-му столбцу)
-        @@For:
+        mov     ax,     0  
+        mov     cx,     [N]
+
+        For:
                 add     ax,     [bx + si]
-				add     si,     dx
-        loop    @@For
-        ;записывает результат в соответсвующую ячейку памяти
+		add     si,     dx
+        loop    For
+
         mov     [Sum],  ax
- 
+
         mov     ax,     4C00h
         int     21h
-main    endp
- 
-end     main
+end
